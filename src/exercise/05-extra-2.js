6ea366d8f687d180d8afb1efd256a88a5067cc38
Parth Kikani <parthkikani02@gmail.com>
2024-05-16 11:57:45 +0300

    complete 05.js extra credit 2

--- src/exercise/05-extra-2.js
+++ src/exercise/05-extra-2.js
@@ -1,9 +1,6 @@
 // State Reducer
 // http://localhost:3000/isolated/exercise/05.js
 
-/* OVERWRITE THE RESPECTIVE EXERCISE SOLUTIONS IN THIS FILE TO VIEW THE PAGE ON THE ABOVE ADDRESS*/
-/* eg: COPY "05-exercise.js" FILE AND PASTE HERE TO LOAD ON THE ABOVE LOCALHOST ADDR */
-
 import * as React from "react";
 import { Switch } from "../switch";
 
